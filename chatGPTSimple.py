23c16d969fd72be0452f6893d49be8e85bd84a82
SK <45126687+KerronManwaring@users.noreply.github.com>
2023-01-16 16:24:33 -0500

    very simple ChatGPT api user interface that speaks

--- chatGPTSimple.py
+++ chatGPTSimple.py
@@ -1,34 +1,47 @@
 import creds
 import os
 import openai
-
+from gtts import gTTS
+from pygame import mixer
+from gtts.tokenizer.pre_processors import abbreviations, end_of_line
 from tkinter import *
 window = Tk()
 from tkinter import messagebox
 
+openai.api_key = creds.OPENAI_API_KEY
+engines = openai.Engine.list()
+#print(engines.data[0].id)
 
 window.title("Simple ChatGPT Interface")
 window.geometry('300x100')
 
 lbl = Label(window, text="request:")
 lbl.grid(column=0, row=1)
+
 txt = Entry(window,width=20)
 txt.grid(column=1, row=1)
 
-openai.api_key = creds.OPENAI_API_KEY
-engines = openai.Engine.list()
-print(engines.data[0].id)
 
+def speak(text):
+    tts = gTTS(text, slow=False, pre_processor_funcs = [abbreviations, end_of_line]) 
+    tts.save('chatGPT.mp3')
+    
+    mixer.init()
+    mixer.music.load("chatGPT.mp3")
+    mixer.music.play()
 
 def request():
     text = txt.get()
     completion = openai.Completion.create(engine="text-davinci-003", prompt=text, max_tokens=1024)
-    #print(text)
-    #print(completion.choices[0].text)
-    messagebox.showinfo('ChatGPT Response', completion.choices[0].text)
+    response = completion.choices[0].text
 
+    speak(response)
+
+    messagebox.showinfo('ChatGPT Response', response) 
         
 btn1 = Button(window, text="Submit to chatGPT", command=request)
 btn1.grid(column=1, row=2) #request
 
+
 window.mainloop()
+
